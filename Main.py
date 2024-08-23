6da7eebf15ec73f07b912a66310cc36aa21f5867
Cody Hatfield <hatfield.coders@gmail.com>
2024-08-23 11:08:08 -0500

    [AMT-2] Heartbeat of mavlink

---
+++ Main.py
@@ -0,0 +1,9 @@
+import pymavlink
+
+time_step = 1 / 20
+
+connection = pymavlink.mavutil.mavutil_connection("udpin:localhost:14569")
+
+connection.wait_heartbeat()
+
+print("Heartbeat for system (system %u component %u)" % (connection.target_system, connection.target_component))
