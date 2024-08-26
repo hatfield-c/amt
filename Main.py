3bc653d1c4af66ba769fea904b53a8e7e442a14d
Cody Hatfield <hatfield.coders@gmail.com>
2024-08-26 02:13:30 -0500

    [AMT-2] Float error

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ rclpy.init()
 
 ros_controller = RosController.RosController()
 
-ros_controller.SetArmed(1)
+ros_controller.SetArmed(1.0)
 time.sleep(0.05)
 
 for i in range(10):
@@ -28,11 +28,11 @@ for i in range(10):
 	print("main:", local_up, local_forward, ros_controller.heading)
 	#input()
 	
-	ros_controller.publish_motor([0, 0, 0, 0])
+	ros_controller.publish_motor([0.1, 0.1, 0.1, 0.1])
 	
 	time.sleep(0.3)
 
-ros_controller.SetArmed(0)
+ros_controller.SetArmed(0.0)
 time.sleep(0.05)
 
 ros_controller.destroy_node()
