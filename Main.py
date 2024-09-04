650f6c077063ea8d0a5db70b09cf1901a9af30e0
Cody Hatfield <hatfield.coders@gmail.com>
2024-09-03 22:01:19 -0500

    [AMT-5] Basic d455 connection script

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import subprocess
 import time
 import rclpy
 import torch
+import traceback
 
 import RosController as RosController
 import Quaternion
@@ -9,49 +10,13 @@ import Transform
 
 from pymavlink import mavutil
 
-connection = mavutil.mavlink_connection("udpin:localhost:14569")
-connection.wait_heartbeat()
-print("Heartbeat for system (system %u component %u)" % (connection.target_system, connection.target_component))
+try:
 
-connection.mav.command_long_send(
-	0, 
-	0, 
-	400, 
-	0,
-	1, 
-	21196, 
-	0, 
-	0, 
-	0, 
-	0,
-	0
-)
+	rclpy.init()
+	ros_controller = RosController.RosController()
 
-connection.mav.command_long_send(
-	0, 
-	0, 
-	187, 
-	0,
-	0.3, 
-	0.3, 
-	0.3, 
-	0.3, 
-	0.3, 
-	0.3,
-	0
-)
-
-#connection.set_actuator(0.3, 0.3, 0.3, 0.3, 0.3, 0.3)
-
-exit()
-
-rclpy.init()
-
-ros_controller = RosController.RosController()
-
-rclpy.spin(ros_controller)
-
-ros_controller.SetArmed(0.0)
-time.sleep(0.05)
-ros_controller.destroy_node()
-rclpy.shutdown()
+	rclpy.spin(ros_controller)
+	ros_controller.destroy_node()
+	rclpy.shutdown()
+except Exception as e:
+	print(traceback.format_exc())
