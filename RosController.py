75420529b37396141c4e331b3cba25c4933e56cd
Cody Hatfield <hatfield.coders@gmail.com>
2024-09-11 03:59:04 -0500

    [AMT-7] Arming switches dropper

--- RosController.py
+++ RosController.py
@@ -11,7 +11,7 @@ from px4_msgs.msg import OffboardControlMode
 from px4_msgs.msg import ManualControlSetpoint
 from px4_msgs.msg import ActuatorMotors
 from px4_msgs.msg import ActuatorServos
-from px4_msgs.msg import VehicleAttitude
+from px4_msgs.msg import VehicleControlMode
 from px4_msgs.msg import VehicleCommand
 from px4_msgs.msg import VehicleThrustSetpoint
 
@@ -25,23 +25,24 @@ class RosController(Node):
 			history = QoSHistoryPolicy.KEEP_LAST,
 			depth = 1
 		)
-
-		self.attitude_sub = self.create_subscription(
-			VehicleAttitude,
-			'/fmu/out/vehicle_attitude',
-			self.attitude_callback,
+		
+		self.control_mode_sub = self.create_subscription(
+			VehicleControlMode,
+			'/fmu/out/vehicle_control_mode',
+			self.ControlModeCallback,
 			qos_profile
 		)
 		
 		self.publisher_offboard_mode = self.create_publisher(OffboardControlMode, '/fmu/in/offboard_control_mode', qos_profile)
-		self.motor_publisher = self.create_publisher(ActuatorMotors, "/fmu/in/actuator_motors", qos_profile)
 		self.servo_publisher = self.create_publisher(ActuatorServos, "/fmu/in/actuator_servos", qos_profile)
 		self.rc_spoofer_publisher = self.create_publisher(ManualControlSetpoint, "/fmu/in/manual_control_input", qos_profile)
 		self.vehicle_command_publisher = self.create_publisher(VehicleCommand, "/fmu/in/vehicle_command", qos_profile)
 		
 		self.quaternion = None
 		self.heading = None
-		
+		self.is_armed = False
+		self.is_dropper_closed = False
+		self.is_offboard = False
 
 		timer_period = 0.02
 		self.timer = self.create_timer(timer_period, self.Update)
@@ -50,68 +51,14 @@ class RosController(Node):
 		self.max_cycles = 10000
 		
 	def Update(self):
-		self.cycles += 1
-		
 		self.PrepareToCommand()
 		
-		if self.cycles < 10:
-			self.SetArmed(1.0)
+		print("[Armed]:", self.is_armed)
+		
+		if not self.is_armed:
 			return
 		
-		t_signal =  (math.sin(self.cycles * (math.pi / 100)) + 1) / 2
-		
-		if t_signal > 0.5:
-			t_signal = 0.7
-		else:
-			t_signal = 0.2
-		
-		self.publish_motor([t_signal, t_signal, t_signal, t_signal])
-		#self.publish_motor([0.341, 0.341, 0.341, 0.341])
-		#self.SetDropperPosition(t_signal)
-		#self.SetManualRcInput(t_signal)
-		
-		if self.cycles > self.max_cycles:
-			self.publish_motor([0.0, 0, 0, 0])
-			self.SetArmed(0.0)
-			exit()
-
-	def SetDropperPosition(self, position_signal):
-		msg = VehicleCommand()
-
-		msg.command = VehicleCommand.VEHICLE_CMD_DO_SET_ACTUATOR 
-		msg.param1 = position_signal
-		msg.param2 = position_signal
-		msg.param3 = position_signal
-		msg.param4 = position_signal
-		msg.param5 = position_signal
-		msg.param6 = position_signal
-		msg.param7 = 0.0
-		
-		msg.timestamp = int(Clock().now().nanoseconds / 1000)
-		msg.target_system = 1
-		msg.target_component = 1
-		msg.source_system = 1
-		msg.source_component = 1
-		msg.from_external = True
-		
-		self.vehicle_command_publisher.publish(msg)
-		
-	def SetManualRcInput(self, position_signal):
-		msg = ManualControlSetpoint()
-		msg.timestamp = int(Clock().now().nanoseconds / 1000)
-		msg.roll = position_signal
-		msg.pitch = position_signal
-		msg.yaw = position_signal
-		msg.throttle = position_signal
-		
-		msg.aux1 = position_signal
-		msg.aux2 = position_signal
-		msg.aux3 = position_signal
-		msg.aux4 = position_signal
-		msg.aux5 = position_signal
-		msg.aux6 = position_signal
-		
-		self.rc_spoofer_publisher.publish(msg)
+		self.cycles += 1
 
 	def PrepareToCommand(self):
 		msg = OffboardControlMode()
@@ -126,61 +73,45 @@ class RosController(Node):
 		
 		self.publisher_offboard_mode.publish(msg)
 
-	def SetArmed(self, arm_value):
+	def ControlModeCallback(self, msg):
+		msg_is_armed = msg.flag_armed
+		msg_is_offboard = msg.flag_control_offboard_enabled
+		
+		if msg_is_armed and not self.is_armed:
+			self.SwitchDropper()
+		
+		self.is_armed = msg_is_armed
+		self.is_offboard = msg_is_offboard
+		
+	def SwitchDropper(self):
+		self.is_dropper_closed = not self.is_dropper_closed
+		
+		self.SetDropper(self.is_dropper_closed)
+		
+	def SetDropper(self, is_closed):
+		servo_signal = 0.0
+		if is_closed:
+			servo_signal = 1.0
+		
 		msg = VehicleCommand()
+
+		msg.command = VehicleCommand.VEHICLE_CMD_DO_SET_ACTUATOR 
+		msg.param1 = servo_signal
+		msg.param2 = servo_signal
+		msg.param3 = servo_signal
+		msg.param4 = servo_signal
+		msg.param5 = servo_signal
+		msg.param6 = servo_signal
+		msg.param7 = 0.0
 		
-		msg.command = VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM
-		msg.param1 = arm_value
-		
+		msg.timestamp = int(Clock().now().nanoseconds / 1000)
 		msg.target_system = 1
 		msg.target_component = 1
 		msg.source_system = 1
 		msg.source_component = 1
 		msg.from_external = True
-		msg.timestamp = int(Clock().now().nanoseconds / 1000)
 		
 		self.vehicle_command_publisher.publish(msg)
-		
-		if arm_value == 1:
-			
-			msg.command = VehicleCommand.VEHICLE_CMD_DO_SET_MODE
-			msg.param1 = 1.0
-			msg.param2 = 6.0
-			msg.timestamp = int(Clock().now().nanoseconds / 1000)
-			
-			self.vehicle_command_publisher.publish(msg)
-		
-	def attitude_callback(self, msg):
-		orientation_q = msg.q
-
-		self.quaternion = orientation_q
-
-	def publish_motor(self, thrusts):
-		msg = ActuatorMotors()
-		
-		msg.timestamp = int(Clock().now().nanoseconds / 1000)
-		msg.control = np.zeros(12, dtype = np.float32)
-		msg.control[0] = thrusts[0]
-		msg.control[1] = thrusts[1]
-		msg.control[2] = thrusts[2]
-		msg.control[3] = thrusts[3]
-		
-		msg.control[4] = thrusts[0]
-		msg.control[5] = thrusts[0]
-		msg.control[6] = thrusts[0]
-		msg.control[7] = thrusts[0]
-		msg.control[8] = thrusts[0]
-		msg.control[9] = thrusts[0]
-		msg.control[10] = thrusts[0]
-		msg.control[11] = thrusts[0]
-		
-		self.motor_publisher.publish(msg)
-		
-		msg = ActuatorServos()
-		msg.timestamp = int(Clock().now().nanoseconds / 1000)
-		msg.control = np.zeros(8, dtype = np.float32) + thrusts[0]
-		
-		self.servo_publisher.publish(msg)
 
 '''
 import time
