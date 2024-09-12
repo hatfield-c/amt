c6e3cbcbe8b1c982ada78640d8b421181f8d95a0
Cody Hatfield <hatfield.coders@gmail.com>
2024-09-12 02:00:23 -0500

    [AMT-7]

---
+++ jetson/autostart_service.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/jetson/miniforge3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/jetson/miniforge3/etc/profile.d/conda.sh" ]; then
+        . "/home/jetson/miniforge3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/jetson/miniforge3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
+if [ -f "/home/jetson/miniforge3/etc/profile.d/mamba.sh" ]; then
+    . "/home/jetson/miniforge3/etc/profile.d/mamba.sh"
+fi
+# <<< conda initialize <<<
+
+mamba activate py3.10
+python /home/jetson/amt/Main.py
