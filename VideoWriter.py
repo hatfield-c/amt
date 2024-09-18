2e5a6f829224879d5add1e74be8344e1088372e7
hatfield-c <hatfield.coders@gmail.com>
2024-09-18 03:16:34 -0500

    [AMT-8]

--- VideoWriter.py
+++ VideoWriter.py
@@ -3,7 +3,7 @@ import cv2
 
 class VideoWriter:
 	def __init__(self):
-		self.video_path = "data/render/d455_render.mp4"
+		self.video_path = "/home/jetson/amt/data/render/d455_render.mp4"
 		self.video_object = cv2.VideoWriter(
 			self.video_path,
 			cv2.VideoWriter_fourcc('m', 'p', '4', 'v'),
