18cf5f25aae821d432f8e0ffb03dc8d212eecc3c
hatfield-c <hatfield.coders@gmail.com>
2024-09-17 19:44:54 -0500

    [AMT-8]

--- Main.py
+++ Main.py
@@ -6,16 +6,16 @@ import traceback
 
 import RosController as RosController
 
-with open('tau_log.txt', "w") as sys.stdout:
-	try:
-	
-		rclpy.init()
-		ros_controller = RosController.RosController()
-		print("<uORB RosController Initialzed!>")
-	
-		rclpy.spin(ros_controller)
-		ros_controller.destroy_node()
-		rclpy.shutdown()
-	except Exception as e:
-		print("Exception occured!")
-		print(traceback.format_exc())
+#with open('tau_log.txt', "w") as sys.stdout:
+try:
+
+	rclpy.init()
+	ros_controller = RosController.RosController()
+	print("<uORB RosController Initialzed!>")
+
+	rclpy.spin(ros_controller)
+	ros_controller.destroy_node()
+	rclpy.shutdown()
+except Exception as e:
+	print("Exception occured!")
+	print(traceback.format_exc())
