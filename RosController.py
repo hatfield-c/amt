0ba38de5de5185eaaaa471e415e5deacb3a9aeda
hatfield-c <hatfield.coders@gmail.com>
2024-09-19 17:14:59 -0500

    [AMT-8]

--- RosController.py
+++ RosController.py
@@ -275,8 +275,7 @@ class RosController(Node):
 		msg_is_armed = msg.flag_armed
 		msg_is_offboard = msg.flag_control_offboard_enabled
 		
-		#if msg_is_offboard and not self.is_offboard:
-		if True:
+		if msg_is_offboard and not self.is_offboard:
 			print("<Offboard control activated!>")
 			self.ResetOrientation()
 			
